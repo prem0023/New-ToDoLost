c1569db13d5a9a6603a5960b5aff43acbf56865d
prem023 <phanishwarnath2@gmail.com>
2023-02-22 22:34:12 +0530

    Project Created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+import ToDoList from './ToDoList';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
 
-function App() {
+const App = () => {
+  const [item, setItem] = useState('');
+  const [newItem, setNewItem] = useState([]);
+
+  const itemEvent = (event) => {
+    setItem(event.target.value);
+  }
+  const addItems = () => {
+    setNewItem((oldVal) => {
+      return [...oldVal, item]
+    });
+    setItem('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className='main_div'>
+        <div className='center_div'>
+          <br />
+          <h1>TODo List</h1>
+          <br />
+          <input type='text' placeholder='Add an Item' onChange={itemEvent} value={item}/>
+          <Button className='newBtn' onClick={addItems}><AddIcon />
+          </Button>
+          <br />
+
+          <ol>
+            {newItem.map((val, id) => {
+              return  (
+                <ToDoList 
+                  key = {id}
+                  text={val}
+                />
+              )
+            })}
+          </ol>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
